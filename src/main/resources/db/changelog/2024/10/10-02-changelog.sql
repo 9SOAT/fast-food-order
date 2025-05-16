76e1f588fb0a3c95572eb7ad48e8bd076561f306
Victor Ambiel <vambiel@fullstacklabs.co>
2025-05-15 21:03:16 -0300

    add payment table

--- src/main/resources/db/changelog/2024/10/10-02-changelog.sql
+++ src/main/resources/db/changelog/2024/10/10-02-changelog.sql
@@ -1,16 +1,16 @@
 -- liquibase formatted sql
 
--- changeset fabiosiqueira:1727992762154-1
+-- changeset fabiosiqueira:1747353323000-1
 ALTER TABLE order_item
     ADD product_category SMALLINT;
 ALTER TABLE order_item
     ADD product_name VARCHAR(255);
 
--- changeset fabiosiqueira:1727992762154-2
+-- changeset fabiosiqueira:1747353323000-2
 ALTER TABLE order_item
     ALTER COLUMN product_category SET NOT NULL;
 
--- changeset fabiosiqueira:1727992762154-4
+-- changeset fabiosiqueira:1747353323000-3
 ALTER TABLE order_item
     ALTER COLUMN product_name SET NOT NULL;
 
