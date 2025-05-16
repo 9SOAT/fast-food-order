d9497cf7702468a3fc433d98d5f46a2b4da3e009
Victor Ambiel <113382997+ambielvictor@users.noreply.github.com>
2025-05-15 21:04:26 -0300

    Add payment table (#6)
    * docs(readme): update descricao do projeto
    
    * add payment table
    
    ---------
    
    Co-authored-by: francesquini88 <68014943+francesquini88@users.noreply.github.com>

--- src/main/resources/db/changelog/2024/10/10-03-changelog.sql
+++ src/main/resources/db/changelog/2024/10/10-03-changelog.sql
@@ -1,14 +1,14 @@
 -- liquibase formatted sql
 
--- changeset fabiosiqueira:1728045287032-1
+-- changeset fabiosiqueira:1747353324000-1
 ALTER TABLE order_item
     DROP COLUMN product_category;
 
--- changeset fabiosiqueira:1728045287032-2
+-- changeset fabiosiqueira:1747353324000-2
 ALTER TABLE order_item
     ADD product_category VARCHAR(255) NOT NULL;
 
--- changeset fabiosiqueira:1728045287032-3
+-- changeset fabiosiqueira:1747353324000-3
 ALTER TABLE order_item
     ALTER COLUMN product_category SET NOT NULL;
 
--- src/main/resources/db/changelog/2024/12/12-01-changelog.sql
+++ src/main/resources/db/changelog/2024/12/12-01-changelog.sql
@@ -1,10 +1,10 @@
 -- liquibase formatted sql
 
--- changeset victorambiel:1734480387800-1
+-- changeset victorambiel:1747353325000-1
 ALTER TABLE orders ADD COLUMN created_at TIMESTAMP WITHOUT TIME ZONE;
 -- rollback ALTER TABLE orders DROP COLUMN created_at;
 
--- changeset victorambiel:1734480409000-2
+-- changeset victorambiel:1747353325000-2
 UPDATE orders
    SET created_at = now();
 
