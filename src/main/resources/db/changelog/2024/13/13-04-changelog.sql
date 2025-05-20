e249e2b5461cc531f3caa3691bb235d3b42c7787
Victor Ambiel <vambiel@fullstacklabs.co>
2025-05-19 22:12:56 -0300

    update payment migration

--- src/main/resources/db/changelog/2024/13/13-04-changelog.sql
+++ src/main/resources/db/changelog/2024/13/13-04-changelog.sql
@@ -1,4 +1,6 @@
 -- liquibase formatted sql
 
 -- changeset victorambiel:1847353329000-1
+ALTER TABLE orders DROP CONSTRAINT FK_ORDERS_ON_PAYMENT;
+ALTER TABLE payment DROP CONSTRAINT pk_payment;
 DROP TABLE payment;
