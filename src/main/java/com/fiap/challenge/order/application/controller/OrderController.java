82b8a66d77ec9349c9116e54ae1dd5b963bd7c32
Victor Ambiel <vambiel@fullstacklabs.co>
2025-05-20 20:45:04 -0300

    return order

--- src/main/java/com/fiap/challenge/order/application/controller/OrderController.java
+++ src/main/java/com/fiap/challenge/order/application/controller/OrderController.java
@@ -33,8 +33,8 @@ public class OrderController {
 
     @ResponseStatus(CREATED)
     @PostMapping("/create")
-    public void saveOrder(@RequestBody Order order) {
-        orderService.saveOrder(order);
+    public Order saveOrder(@RequestBody Order order) {
+        return orderService.saveOrder(order);
     }
 
     @GetMapping()
